18323f2af8d3b7c30e95eae938da06b068e1e2c5
Szymon Madeja <szmadeja@c2r5s3.42warsaw.pl>
2025-01-27 20:13:00 +0100

    sdf

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,37 @@
 /*   By: szmadeja <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 18:25:41 by szmadeja          #+#    #+#             */
-/*   Updated: 2025/01/21 18:18:45 by szmadeja         ###   ########.fr       */
+/*   Updated: 2025/01/27 20:12:51 by szmadeja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*lineread(char *storage, int fd)
+{
+	int		i;
+	char	*temp;
+
+	i = 1
+	while (i > 0)
+	{
+		temp = malloc(BUFFER_SIZE + 1);
+		if (!temp)
+			return (NULL);
+		i = read(fd, temp, BUFFER_SIZE);
+		if (i == -1)
+		{
+			free (temp);
+			return (NULL);
+		}
+		temp[i] = '\0';
+		if (strchr(storage, '\n'))
+			break ;
+
+		joinfree(storage, temp);
+	}
+	return (storage);
+}
 
 char	*get_next_line(int fd)
 {
@@ -19,4 +45,3 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
-
